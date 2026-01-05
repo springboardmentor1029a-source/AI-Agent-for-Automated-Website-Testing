076386f404499281ae269d13ac83d2a8375901ff
harshika1902-glitch <harshikareddy1437@gmail.com>
2026-01-05 13:27:37 +0530

    Create workflow.py

---
+++ Milestone2/workflow.py
@@ -0,0 +1,14 @@
+# workflow.py
+
+def generate_test_steps(actions):
+    steps = []
+
+    for act in actions:
+        if act["action"] == "open":
+            steps.append("Open the webpage")
+        elif act["action"] == "click":
+            steps.append("Click on the button")
+        elif act["action"] == "type":
+            steps.append("Type into the input field")
+
+    return steps
