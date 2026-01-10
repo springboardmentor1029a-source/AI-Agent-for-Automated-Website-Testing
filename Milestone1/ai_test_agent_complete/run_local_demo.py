4dd5c192f13a108b0b7260d1bcf133c3b3360968
Bindu.A <abindu074@gmail.com>
2026-01-10 11:23:30 +0530

    Delete Milestone1/ai_test_agent_complete directory

--- Milestone1/ai_test_agent_complete/run_local_demo.py
+++
@@ -1,4 +0,0 @@
-from agent.workflow import run_agent_test
-if __name__ == '__main__':
-    instr = "Open demo page, enter username admin and password pass, click login and expect Login successful"
-    print(run_agent_test(instr))
